6ce65a0ed9231d26b1e3b63e65e9f2d98062f37c
Rayná Reveles <rayna.uni@gmail.com>
2021-04-06 10:20:06 -0300

    Create TesterCarro.java

---
+++ Semana 4 - Exercício Herança/TesterCarro.java
@@ -0,0 +1,12 @@
+public class TesterCarro {
+
+    public static void main(String[] args){
+
+        BMW carro = new BMW(75.5);
+
+        carro.setCor("Prata");
+        carro.setNum_acentos(5);
+        carro.imprimir();
+    }
+    
+}
